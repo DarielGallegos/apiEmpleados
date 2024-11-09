2796d6b96d6c57c2280867261d3fa116785a6975
DempseyPrimi <joangallegos39@gmail.com>
2024-11-08 22:05:19 -0600

    Change rutes api

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const routerEmployee = require('./routes/EmpleadosRouter.js')
 const routerLogin = require('./routes/LoginRouter.js')
 
 // Middleware
-app.use("/api/empleados/", routerEmployee)
-app.use("/api/auth/", routerLogin)
+app.use("/empleados/", routerEmployee)
+app.use("/auth/", routerLogin)
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
--- controllers/LoginCtrl.js
+++ controllers/LoginCtrl.js
@@ -6,7 +6,7 @@ const login = async (req, res) => {
     const { username, password } = req.query;
     connection.query(`CALL getAccess('${username}', '${password}')`, (err, result, fld) => {
         if(result[0].length > 0){
-            const token = jwt.sign({username: username }, "Tokent", { expiresIn: 2*60},(err, token) => {
+            const token = jwt.sign({username: username }, "Tokent", { expiresIn: 10*60},(err, token) => {
                 token ? res.status(200).json({token}) : res.status(500).json({message: err.message})
             })
         }
